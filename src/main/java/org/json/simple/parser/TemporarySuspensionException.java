1a98ea47ad9aee804b0d9cd5d9de48c6f7ed0149
Ted Park <steadiest@naver.com>
2016-03-30 11:23:58 +0900

    Create TemporarySuspensionException.java

---
+++ src/main/java/org/json/simple/parser/TemporarySuspensionException.java
@@ -0,0 +1,10 @@
+package org.json.simple.parser;
+
+/**
+ * This exception can be used to hold parser status
+ * User: kgn
+ * Date: 2016. 3. 30.
+ * Time: 오전 11:21
+ */
+public class TemporarySuspensionException extends RuntimeException {
+}
